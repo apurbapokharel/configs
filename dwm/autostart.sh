f3851c1fe6ea86b82b098cbfc06d4b747ea80d81
barunpradhan <barunpradhan987@gmail.com>
2021-08-06 10:36:13 +0545

    master: Replace paths from arco-dwm to dw

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -6,7 +6,7 @@ function run {
     $@&
   fi
 }
-run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
+# run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
 #run "xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal"
 #run "xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
 #run xrandr --output eDP-1 --primary --mode 1368x768 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
@@ -24,11 +24,11 @@ run "xfce4-power-manager"
 #run "blueberry-tray"
 run "/usr/lib/xfce4/notifyd/xfce4-notifyd"
 run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
-picom -b  --config ~/.config/arco-dwm/picom.conf &
+picom -b --config ~/.config/dwm/picom.conf &
 run "numlockx on"
 run "volumeicon"
 run slstatus &
-sxhkd -c ~/.config/arco-dwm/sxhkd/sxhkdrc &
+sxhkd -c ~/.config/dwm/sxhkd/sxhkdrc &
 #run "nitrogen --restore"
 # run "conky -c $HOME/.config/arco-dwm/system-overview"
 #you can set wallpapers in themes as well
